65593736bd793d957da7dce3c6fa70051c84dffe
glz-diego <glzdiego12@gmail.com>
2019-03-28 23:30:25 -0500

    init message

--- script.js
+++ script.js
@@ -1,78 +1,24 @@
-let searchTerms = [];
+const todos = [];
+var iDiv;
+function add(){
+	let task = document.querySelector("#newTask").value;
+	if (task != "") {
+		document.querySelector("#newTask").value = "";
+	}
+	iDiv = document.createElement('div');
+	iDiv.innerHTML = "<input id='todo${item.itemid}' type='checkbox'/>&nbsp;" + task;
+	document.querySelector('#entries').appendChild(iDiv);
 
-		function doSearch() {
-			//clears all output in the console
-			console.clear();
+	let inputData = document.getElementById("newTask");
 
-			//start timer
-			console.time("doSearch");
+	todos.push(inputData.value);
 
-			//get a reference to the text box
-			let txtSearch = document.querySelector("#txtSearch");
-
-			//get a reference to the checkbox
-			let chkAlert = document.querySelector("#chkAlert");
-
-			// add this new search term to the array
-			searchTerms.push({"searchTerm" : txtSearch.value});
-
-			//if the checkbox is checked then show an alert
-			if(chkAlert.checked) {
-				//pop up the value of txtSearch
-				alert(`You added the task of ${txtSearch.value}`); 
-			}
-
-			//keeps track of the calls to txtSearch
-			console.count("txtSearch count", txtSearch);
-			
-			//starts a collapsed group of log entries
-			console.groupCollapsed("log examples");
-				
-				//outputs the value in the textbox
-				console.log(txtSearch.value);
-
-				//outputs the object that txtSearch references
-				console.dir(txtSearch);
-
-				//outputs the value as an error
-				console.error(txtSearch.value);
-
-				//outputs the value as a warning
-				console.warn(txtSearch.value);
-
-				//outputs the value as info
-				console.info(txtSearch.value);
-
-				//outputs the value as debug info
-				console.debug(txtSearch.value);
-
-				//checks that the value is "frogs", otherwise prints error
-				console.assert(txtSearch.value == "frogs", "Search value should be frogs");
-
-			//ends the group, all consoles after this are outside the group
-			console.groupEnd();
-
-			//keeps track of the calls to txtSearch
-			console.count("txtSearch count", txtSearch);
-
-			//get a reference to the output div and history
-			let searchOutput = document.querySelector("#searchOutput");
-			let searchHistory = document.querySelector("#searchHistory");
-			
-			//write the search term to the output div
-			searchOutput.innerHTML = `You added the task of ${txtSearch.value}`;
-			searchHistory.innerHTML = '';
-
-			for(var i = 0; i < searchTerms.length; i++) {
-				searchHistory.innerHTML = searchHistory.innerHTML + "<BR>" + searchTerms[i].searchTerm;
-			}
-
-			//print the search terms used so far
-			console.table(searchTerms);
-
-			//end the timer
-			console.timeEnd("doSearch");
-
-			//stops the form from submitting!!
-			return false;
-		}
+	console.log(todos);		
+}
+function clearAll() {
+  var checkBox = document.getElementById("todo${item.itemid}");
+  let inputData = document.getElementById("newTask");
+  if (checkBox.checked == true){
+		iDiv.innerHTML = "";
+  }
+}
